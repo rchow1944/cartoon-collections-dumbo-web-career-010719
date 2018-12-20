296bb796818160813ef857fa205192dab7a16e2d
Raymond Chow <raychow1944@yahoo.com>
2018-12-20 23:13:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(elements)# code an argument here
   # Your code here
+  elements.collect do |el|
+    el.capitalize!
+  end
 end
 
 def long_planeteer_calls# code an argument here

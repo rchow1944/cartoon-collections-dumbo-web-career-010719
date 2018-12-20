f4442af0f53b95625e1ac3f0fff0a1d3503fc2a0
Raymond Chow <raychow1944@yahoo.com>
2018-12-20 23:15:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,11 @@ def summon_captain_planet(elements)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? do |call|
+    call.size > 4
+  end
 end
 
 def find_the_cheese# code an argument here
